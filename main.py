ed3e7b2b75d87772add5d1a0d596333a5b2ae11d
Phillip Nyarko <phillip.k.nyarko@gmail.com>
2022-05-12 21:06:39 -0500

    Title Bar fix 3

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ def title_bar():
 
     """render settings button"""
     settings_btn = pygame.image.load("settings_icon.png")
-    settings_btn_rect = pygame.Rect((0, 0), (menu_btn_pos + 1300, title_bar_rect.height))
+    settings_btn_rect = pygame.Rect((0, 0), (100 + 1300, title_bar_rect.height))
     settings_btn_rect_center_pos = (settings_btn_rect.width // 2, settings_btn_rect.height // 2)
     settings_btn_center = menu_btn.get_rect(center=settings_btn_rect_center_pos)
     screen.blit(settings_btn, settings_btn_center)
